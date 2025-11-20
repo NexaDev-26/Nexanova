5eec949985ca5fc8e53c52ba7c07a838475de93b
NexaDev-26 <64757038+NexaDev-26@users.noreply.github.com>
2025-11-21 01:35:28 +0300

    Update ThemeContext.js

--- frontend/src/context/ThemeContext.js
+++ frontend/src/context/ThemeContext.js
@@ -1,4 +1,8 @@
 import React, { createContext, useState, useContext } from 'react';
+import api from '../utils/api';
+
+api.post('/api/auth/register', userData)
+
 const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 export const ThemeProvider = ({ children }) => {
